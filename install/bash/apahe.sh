2aafb515f5c1955c7c9c28ecf0bf73b4c35b77a9
seanmccann93 <seanmccann93@outlook.com>
2021-04-04 20:18:04 +0100

    'Upload Nginx install and script + notes' ~ 04/04/21 @ 20:18:04

--- install/bash/apahe.sh
+++ install/bash/apahe.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
+
+# Gather required Upadtes
 apt update && apt upgrade -y
+
+# Enable Uncomplicated Firewall
 echo "y" | ufw enable
+
+# Install Apache2
 apt install apache2 -y
+
+# Allow Ports to be opened
 ufw allow 'Apache'
 ufw allow 'OpenSSH'
+
+# Restart Apache to launch application
 systemctl restart apache2
+
+# Restart system to confirm changes made to the ports
 reset
