5589274d3df877170f13e23456916d35f80f9359
seanmccann93 <seanmccann93@outlook.com>
2021-04-06 23:22:42 +0100

    'Docker Build Image Tool (DockerCreate) ~ To help build docker image in current diretory.' ~ 04/06/21 @ 23:22:42

--- plugs/dc.sh
+++ plugs/dc.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+SpaceTest=false
+Hint="(Y/N)"
+
 # Docker Create Image Plug ~ dcreate
 echo " "
 echo "List of current Docker Images"
@@ -12,12 +15,7 @@ echo " "
 echo "Please, only use '-', '_' or CamulCase for spacing in naming!"
 echo " "
 read -p "What is the name of this new 'image'? - " ImgName
-read -p "What tag? (i.e. version-*, v*, *.)    - " ImgTag
 echo " "
-
-
-SpaceTest=false
-
 while [ $SpaceTest = false ]
 do
     case "$ImgName" in  
@@ -30,51 +28,66 @@ do
             echo " "
             echo "is not a valid input recomended for this tool."
             echo "Spaces can run potentially run additional commands."
-            read -p "Do you wish to continue (Y/N)? " yn # Optional Pull of git changes
-                case $yn in
-                    [Yy]* ) 
-                        $SpaceTest=true
-                        break;;
-                    [Nn]* )
-                        echo " "
-                        read -p "What is the new name of the 'image'? - " ImgName
-                        echo " ";;
-                    * )
-                        echo "Please answer with 'y' or 'n'."
-                    ;;
-                esac
-            ;;
-        *)
-            $SpaceTest=true
-            ;;
-    esac
-done
-
-case "$ImgTag" in  
-     *\ * )
-        echo "Warning !"
-        echo " "
-        echo "'docker build -t $ImgName:$ImgTag'"
-        echo " "
-        echo "is not a valid input recomended for this tool."
-        echo "Spaces can run potentially run additional commands."
-        read -p "Do you wish to continue (Y/N)? " yn # Optional Pull of git changes
+            read -p "Do you wish to continue $Hint? " yn # Optional Pull of git changes
             case $yn in
                 [Yy]* ) 
+                    echo " "
+                    $SpaceTest = true
+                    Hint="(Y/N)"
                     break;;
                 [Nn]* )
                     echo " "
-                    read -p "What is the new name of the 'image Tag'? - " ImgTag
-                    echo " "
-                    break;;
+                    Hint="(Y/N)"
+                    read -p "What is the new name of the 'image'? - " ImgName
+                    echo " ";;
                 * )
-                    echo "Please answer with 'y' or 'n'."
+                    Hint="(Please answer with 'Yy' or 'Nn' only!)"
+                    echo " ";;
+            esac
+            ;;
+        *)
+            $SpaceTest = true
+            break;;
+    esac
+done
+
+read -p "What tag? (i.e. version-*, v*, *.)    - " ImgTag
+
+$SpaceTest = false
+
+while [ $SpaceTest = false ]
+do
+    case "$ImgTag" in  
+        *\ * )
+            echo "----------------------------------------------------------------------------"
+            echo "Warning !"
+            echo "----------------------------------------------------------------------------"
+            echo " "
+            echo "'docker build -t $ImgName:$ImgTag'"
+            echo " "
+            echo "is not a valid input recomended for this tool."
+            echo "Spaces can run potentially run additional commands."
+            read -p "Do you wish to continue $Hint? " yn # Optional Pull of git changes
+            case $yn in
+                [Yy]* ) 
+                    echo " "
+                    $SpaceTest = true
+                    break;;
+                [Nn]* )
+                    echo " "
+                    Hint="(Y/N)"
+                    read -p "What is the new name of the 'image Tag'? - " ImgTag
+                    echo " ";;
+                * )
+                    Hint="(Please answer with 'Yy' or 'Nn' only!)"
+                    echo " "
                 ;;
             esac
-          ;;
-       *)
-           echo "NoSpacesInTag"
-           ;;
-esac
+            ;;
+        *)
+            $SpaceTest = true
+            break;;
+    esac
+done
 
-echo "docker build -t $ImgName:$ImgTag"
+docker build -t $ImgName:$ImgTag
